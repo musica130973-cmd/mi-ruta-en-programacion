bddb2a1b6dcdb62471e7738df44fcf29ddb8d784
musica130973-cmd <musica130973@gmail.com>
2026-03-16 22:37:33 +0100

    Update Ejercicio2.java

--- 01-java/ejercicios java/04-ejercicios comentarios/Ejercicio2.java
+++ 01-java/ejercicios java/04-ejercicios comentarios/Ejercicio2.java
@@ -1,4 +1,5 @@
-//Crea un programa que imprima tu nombre y añade un comentario multilínea explicando qué hace el programa
+// Ejercicio 2
+// Crea un programa que imprima tu nombre y añade un comentario multilínea explicando qué hace el programa
 
 public class Ejercicio2 {
     public static void main(String[] args) {
